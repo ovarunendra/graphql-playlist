f13d66799214a513a2caa6ea49a12fb65f27dc88
ovarunendra <vpsingh016@gmail.com>
2018-04-14 20:08:52 +0530

    Making Queries from React

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import BookList from './components/BookList';
 
 // apollo client setup
 const client = new ApolloClient({
-  uri: 'http://localhost:400/graphql'
+  uri: 'http://localhost:4000/graphql'
 });
 
 class App extends Component {
--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// allow cross-origin request
+app.use(cors());
 
 mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds135946.mlab.com:35946/gql-dev');
 mongoose.connection.once('open', () => {
