1aa2dee155a4cfdc9e9206d1d2f39e36b0ee9bd3
ovarunendra <vpsingh016@gmail.com>
2018-06-12 07:45:54 +0530

    add pagination

--- server/schema/healthandwellnessprogramViewer.js
+++ server/schema/healthandwellnessprogramViewer.js
@@ -3,17 +3,24 @@ const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLList,
+  GraphQLInt,
 } = graphql;
 
 const { PostType } = require('./types');
+
+const BASE_URL = 'http://healthandwellnessprogram.us';
 
 const HealthandwellnessprogramViewerType = new GraphQLObjectType({
   name: 'HealthandwellnessprogramViewer',
   fields: () => ({
     posts: {
       type: new GraphQLList(PostType),
+      args: {
+        page: { type: GraphQLInt, defaultValue: 1 }
+      },
       resolve(parent, args) {
-        const url = 'http://healthandwellnessprogram.us/wp-json/wp/v2/posts';
+        const { page } = args;
+        const url = `${BASE_URL}/wp-json/wp/v2/posts?page=${page}`;
         let promises = [];
         return axios.get(url).then(response => {
           const { data } = response;
